e61582a1bd246ad4548e3e4c24315582397a4040
Alistair Young <avatar@arkane-systems.net>
2026-04-02 13:48:05 -0500

    Add string and type extension methods; update settings
    Added ReSharper setting to skip 'extensions' folders. Introduced string extension methods (UpTo, RemoveNonAlphanumeric) using generated regex. Added IsDerivedFromGenericType extension for Type.

---
+++ Arkane.Core/Extensions/ExtensionMethods-System-String.cs
@@ -0,0 +1,65 @@
+﻿#region header
+
+// Arkane.Core - ExtensionMethods-System-String.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2018.  All rights reserved.
+// 
+// Created: 2026-04-02 1:00 PM
+
+#endregion
+
+#region using
+
+using System.Text.RegularExpressions;
+
+using JetBrains.Annotations;
+
+#endregion
+
+#pragma warning disable IDE0130
+namespace ArkaneSystems.Arkane;
+
+/// <summary>
+///   Extension methods host class.
+/// </summary>
+
+// This part of the extension methods class is reserved for extension methods on System.String.
+public static partial class ExtensionMethods
+{
+  #region Nested type: $extension
+
+  extension (string @this)
+  {
+    [PublicAPI]
+    public string UpTo (int maxLength, string suffixToUseWhenTooLong = "...")
+    {
+      if (string.IsNullOrEmpty (@this))
+        return string.Empty;
+
+      return @this.Length > maxLength
+               ? string.Concat (str0: @this.AsSpan (start: 0, length: maxLength), str1: suffixToUseWhenTooLong)
+               : @this;
+    }
+
+    [PublicAPI]
+    public string RemoveNonAlphanumeric ()
+    {
+      MatchCollection matchCollection = AlphanumericRegex ().Matches (input: @this);
+      string          result          = string.Concat (matchCollection.Select (static m => m.Value));
+
+      return result;
+    }
+  }
+
+  #endregion
+
+  #region Supporting regexes for string extension methods
+
+  [GeneratedRegex ("[A-Za-z0-9]+")]
+  private static partial Regex AlphanumericRegex ();
+
+  #endregion Supporting regexes for string extension methods
+}
---
+++ Arkane.Core/Extensions/ExtensionMethods-System-Type.cs
@@ -0,0 +1,46 @@
+﻿#region header
+
+// Arkane.Core - ExtensionMethods-System-Type.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2018.  All rights reserved.
+// 
+// Created: 2026-04-02 1:25 PM
+
+#endregion
+
+#region using
+
+using JetBrains.Annotations;
+
+#endregion
+
+#pragma warning disable IDE0130
+namespace ArkaneSystems.Arkane;
+
+/// <summary>
+///   Extension methods host class.
+/// </summary>
+
+// This part of the extension methods class is reserved for extension methods on System.Type.
+public static partial class ExtensionMethods
+{
+  #region Nested type: $extension
+
+  extension (Type @this)
+  {
+    [PublicAPI]
+    public bool IsDerivedFromGenericType (Type genericType)
+    {
+      if (@this == typeof (object))
+        return false;
+
+      return (@this.IsGenericType && (@this.GetGenericTypeDefinition () == genericType)) ||
+             @this.BaseType!.IsDerivedFromGenericType (genericType);
+    }
+  }
+
+  #endregion
+}
