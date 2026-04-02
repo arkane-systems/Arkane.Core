0dc97bb032da9804befcce005371eaf02a1c8924
Alistair Young <avatar@arkane-systems.net>
2026-04-02 12:49:57 -0500

    Add IGenericMetadata interface and update project folders
    Added IGenericMetadata.cs with a generic MEF metadata interface using JetBrains.Annotations. Updated Arkane.Core.csproj to replace the "Annotations" folder with "Aspects".

---
+++ Arkane.Core/ComponentModel/Composition/IGenericMetadata.cs
@@ -0,0 +1,32 @@
+﻿#region header
+
+// Arkane.Core - IGenericMetadata.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2018.  All rights reserved.
+// 
+// Created: 2026-04-02 12:46 PM
+
+#endregion
+
+#region using
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace ArkaneSystems.Arkane.ComponentModel.Composition;
+
+/// <summary>
+///   A generic metadata interface for use with MEF.
+/// </summary>
+[PublicAPI]
+public interface IGenericMetadata
+{
+  /// <summary>
+  ///   Name of this MEF part.
+  /// </summary>
+  string Name { get; }
+}
