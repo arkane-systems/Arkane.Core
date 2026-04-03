2742e6365f6969580b6c703b314b68aec49d4770
Alistair Young <avatar@arkane-systems.net>
2026-04-03 10:05:20 -0500

    Comments on IDisposable.

--- Arkane.Core/IDisposable.cs
+++ Arkane.Core/IDisposable.cs
@@ -5,7 +5,7 @@
 // Alistair J. R. Young
 // Arkane Systems
 // 
-// Copyright Arkane Systems 2012-2018.  All rights reserved.
+// Copyright Arkane Systems 2012-2026.  All rights reserved.
 // 
 // Created: 2026-04-02 11:05 PM
 
@@ -19,6 +19,16 @@ using JetBrains.Annotations;
 
 namespace ArkaneSystems.Arkane;
 
+/// <summary>
+///   Defines a disposable wrapper that exposes an underlying value.
+/// </summary>
+/// <typeparam name="T">
+///   The type of value exposed by this disposable instance.
+/// </typeparam>
+/// <remarks>
+///   This interface extends <see cref="System.IDisposable" /> and uses covariance
+///   so implementations can be used where a less-derived <typeparamref name="T" /> is expected.
+/// </remarks>
 [PublicAPI]
 public interface IDisposable<out T> : IDisposable
 {
