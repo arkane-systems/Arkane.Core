023137894b80105374bc80d447a46b6720a574b3
Alistair Young <avatar@arkane-systems.net>
2026-04-06 09:32:36 -0500

    Add Memento class and Action.AsMemento extension
    Introduce sealed Memento class to encapsulate an Action for deferred execution on disposal, implementing IDisposable. Add AsMemento() extension method for Action, enabling easy conversion to a disposable Memento. Both are annotated with [PublicAPI] and include XML documentation.

---
+++ Arkane.Core/Extensions/ExtensionMethods-System-Action.cs
@@ -0,0 +1,45 @@
+﻿#region header
+
+// Arkane.Core - ExtensionMethods-System-Action.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2026.  All rights reserved.
+// 
+// Created: 2026-04-06 9:27 AM
+
+#endregion
+
+#region using
+
+using JetBrains.Annotations;
+
+#endregion
+
+#pragma warning disable IDE0130
+namespace ArkaneSystems.Arkane;
+
+/// <summary>
+///   Extension methods host class.
+/// </summary>
+public static partial class ExtensionMethods
+{
+  #region Nested type: $extension
+
+  /// <summary>
+  ///   Extension methods for System.Action.
+  /// </summary>
+  extension (Action @this)
+  {
+    /// <summary>
+    ///   Converts the Action into a <see cref="T:ArkaneSystems.Arkane.Memento" /> which will cause it to be executed
+    ///   when the Memento is disposed.
+    /// </summary>
+    /// <returns>The produced Memento.</returns>
+    [PublicAPI]
+    public IDisposable AsMemento () => new Memento (@this);
+  }
+
+  #endregion
+}
---
+++ Arkane.Core/Memento.cs
@@ -0,0 +1,38 @@
+﻿#region header
+
+// Arkane.Core - Memento.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2026.  All rights reserved.
+// 
+// Created: 2026-04-06 9:13 AM
+
+#endregion
+
+#region using
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace ArkaneSystems.Arkane;
+
+/// <summary>
+///   Remember an action to be performed on disposal; returned from a method needing later closure.
+/// </summary>
+[PublicAPI]
+public sealed class Memento (Action action) : IDisposable
+{
+  private Action Action { get; init; } = action;
+
+  private bool Disposed { get; set; }
+
+  void IDisposable.Dispose ()
+  {
+    // TODO: Add assume contract that this.Disposed is false, and throw if not.
+    this.Disposed = true;
+    this.Action ();
+  }
+}
